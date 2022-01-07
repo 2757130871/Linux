2b1a9c44032715953ac1cdf1c5b1d797b9a7fa25
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-01-07 20:50:19 +0800

    Add files via upload

---
+++ process/proc_killfunc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <signal.h>
+#include <unistd.h>
+int main()
+{
+
+
+    int pid = fork();
+
+    if(0 == pid)
+    {
+        while(1)
+        {
+            printf("child on work..\n");
+            sleep(1);
+        }
+        return 0;
+    }
+
+    printf("准备杀死子进程\n");
+    sleep(4);
+
+    kill(pid, 15);
+
+
+}

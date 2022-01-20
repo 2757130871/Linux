7c926501d05750c2a0225c335bb938dd36e5fb46
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-01-21 01:32:09 +0800

    Update volatile1.c

--- bit/signal/volatile1.c
+++ bit/signal/volatile1.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #include <signal.h>
 
-//volatile¹Ø¼ü×Ö±£´æ±äÁ¿µÄ¿É¼ûÐÔ£¬ÓÀÔ¶¶ÁÈ¡µÄÊÇÄÚ´æÖÐ×îÐÂÖµ¡£
+//volatile 保持内存的可见性，如果程序被编译器优化的很厉害，那么有一些变量就可能一直存在寄存器中，cpu不会一直去内存中访问，如果此时有另外一个
+//执行流修改了这个值，这对于cpu是不可见的。所以这里需要用到volatile关键字。
 volatile int flag = 0;
 
 void handler(int sig)

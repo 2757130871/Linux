3bb2510d9b51a6230e3baf2fb6bf1afe6a012edd
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-02-18 23:23:54 +0800

    Add files via upload

---
+++ bit/socket/tcp_echo_server/HttpServers/demo1/main.cpp
@@ -0,0 +1,18 @@
+#include "servers.hpp"
+
+
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2){
+        printf("input error\n");
+        exit(-1);
+    }
+
+    int serv_port = atoi(argv[1]);
+    
+    HttpServer server(serv_port);
+    server.init();
+    server.start();
+}
+    

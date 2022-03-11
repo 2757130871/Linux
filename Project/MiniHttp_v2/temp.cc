016535fc10e805e428126b03688dcb3ee3ecafd1
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-03-11 22:22:25 +0800

    Add files via upload

---
+++ Project/MiniHttp_v2/temp.cc
@@ -0,0 +1,33 @@
+
+#define HTTP_VERSION "1"
+
+void func()
+{
+//     auto &response_body = http_response.response_body;
+//     size_t size = 0;
+//     size_t total = 0;
+//     const char *start = response_body.c_str();
+// while( total < response_body.size() && (size = send( sock，start + total,response_body.size(){
+//         total += size;
+
+// }
+}
+
+// void BuildHttpResponseHelper(int code)
+// {
+//     //http_request;//http_response;
+//     auto &status_line = http_response.status_line;
+//     status_line += HTTP_VERSION;
+//     status_line += " ";
+//     status_line = std::to_string(code);
+//     status_line += " ";
+//     status_line += Code2Desc(code);
+//     switch (code)
+//     {
+//     case 404:
+//         break;
+
+//     default:
+//         break;
+//     }
+// }

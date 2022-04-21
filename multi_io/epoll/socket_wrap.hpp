27a70b6a5e0a3e0be09fc41bebb55610611a2376
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-04-21 14:45:40 +0800

    Add files via upload

---
+++ multi_io/epoll/socket_wrap.hpp
@@ -0,0 +1,91 @@
+
+#include <iostream>
+
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/stat.h>
+
+#include <arpa/inet.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include "Util.hpp"
+
+using namespace std;
+
+int Socket()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        LOG(ERROR) << "SOCKET ERROR" << endl;
+        exit(-1);
+    }
+    int opt = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    return sock;
+}
+
+void Bind(int lsock, int port)
+{
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(lsock, (struct sockaddr *)&addr, sizeof(addr)) != 0)
+    {
+        LOG(ERROR) << "BIND ERROR" << endl;
+        exit(-1);
+    }
+}
+
+void Listen(int lsock, int backlog)
+{
+    if (listen(lsock, backlog) != 0)
+    {
+        LOG(ERROR) << "LISTEN ERROR" << endl;
+        exit(-1);
+    }
+}
+
+int Accept(int lsock)
+{
+    int ret = accept(lsock, NULL, NULL);
+    if (ret < 0)
+    {
+        LOG(ERROR) << "ACCEPT ERROR" << endl;
+        exit(-1);
+    }
+    return ret;
+}
+
+
+void Connect(int lsock, int port, const char* serv_ip)
+{
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr =  inet_addr(serv_ip);
+    socklen_t len = sizeof(addr);
+    if(connect(lsock, (struct sockaddr*)&addr, len) != 0)
+    {
+        LOG(ERROR) << "CONNECT ERROR" << endl;
+        exit(-1);
+    }
+}
+
+
+void SetNonBlock(int fd)
+{
+    assert(fd >= 0);
+    int mode = fcntl(fd, F_GETFL);
+    fcntl(fd, F_SETFL, mode | O_NONBLOCK);
+}
+

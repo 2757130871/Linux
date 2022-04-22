79d44ec9a724f7fca702109709242acc735d7c6f
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-04-23 01:52:11 +0800

    Create client_test.cc

---
+++ multi_io/epoll/client_test.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("input error\n");
+        exit(1);
+    }
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    int ret = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (ret < 0)
+    {
+        printf("bind error\n");
+        exit(1);
+    }
+
+    char buf[64];
+
+    char c = 'a';
+    while (1)
+    {
+        if (c >= 'z')
+            c = 'a';
+        write(sock, &c, 1);
+        
+        sleep(1);
+        c++;
+    }
+}

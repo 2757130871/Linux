9424647723a7a8c87ba4453f78502f5be24a93fd
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-04-24 14:43:15 +0800

    Create reactor_ET.cc

---
+++ multi_io/epoll_reactor/ET/reactor_ET.cc
@@ -0,0 +1,54 @@
+#include <signal.h>
+
+#include "CallBack.hpp"
+
+#include "TcpServer.hpp"
+
+void Usage()
+{
+    cout << "Usage: ./server port" << endl;
+    exit(-1);
+}
+
+//EPOLL reactor + LT
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        Usage();
+
+    //防止服务器被SIGPIPE信号终止
+    signal(SIGPIPE, SIG_IGN);
+
+    //
+    int port = atoi(argv[1]);
+    TcpServer *tcp_server = TcpServer::GetInstance(port);
+    int lsock = tcp_server->GetLsock();
+
+    //创建eoll模型
+    int epfd = epoll_create(1);
+
+    //添加lsock
+    struct epoll_event ent;
+    ent.events = EPOLLIN;
+
+    ent.data.ptr = new CallBack(epfd, lsock, ent.events, GET_LINK);
+    epoll_ctl(epfd, EPOLL_CTL_ADD, lsock, &ent);
+
+    struct epoll_event evs[1024];
+
+    while (1)
+    {
+        int nready = epoll_wait(epfd, evs, 1024, -1);
+        if (nready < 0)
+        {
+            LOG(FATAL) << "epoll_wait Error !" << endl;
+            continue;
+        }
+
+        for (int i = 0; i < nready; i++)
+        {
+            CallBack *call_back = static_cast<CallBack *>(evs[i].data.ptr);
+            (*call_back)(); //调用回调
+        }
+    }
+}

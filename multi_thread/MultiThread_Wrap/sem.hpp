44d3d88e57dde818cdf3b348ee812422569cc1f3
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-04-29 01:00:41 +0800

    Create sem.hpp

---
+++ multi_thread/MultiThread_Wrap/sem.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <cassert>
+
+namespace cxr
+{
+
+    class sem
+    {
+    public:
+        sem(int sum)
+        {
+            assert(sem_init(&_sem1, 0, sum) == 0);
+        }
+
+        ~sem()
+        {
+            assert(sem_destroy(&_sem1) == 0);
+        }
+
+        bool P()
+        {
+            return sem_wait(&_sem1) == 0;
+        }
+
+        bool V()
+        {
+            return sem_post(&_sem1) == 0;
+        }
+
+    private:
+        sem_t _sem1;
+    };
+} // namespace  cxr

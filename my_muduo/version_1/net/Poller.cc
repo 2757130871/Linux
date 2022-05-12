33b88d1758e5c2ffec8ea4496b1cabd299b5ac6d
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-05-12 21:36:01 +0800

    Create Poller.cc

---
+++ my_muduo/version_1/net/Poller.cc
@@ -0,0 +1,10 @@
+
+#include "Poller.h"
+
+Poller::Poller(EventLoop *loop) : ownerLoop_(loop) {}
+
+bool Poller::HasChannel(Channel *channel) const
+{
+    auto it = channels_.find(channel->Get_Fd());
+    return it != channels_.end() && it->second == channel;
+}

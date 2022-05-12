3321709c09206c103671893c7f4cb616b1a07bac
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-05-12 21:35:21 +0800

    Create Poller.h

---
+++ my_muduo/version_1/net/Poller.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+#include "../base/noncopyable.hpp"
+#include "../base/TimeStamp.hpp"
+#include "Channel.hpp"
+
+class EventLoop;
+class Channel;
+// Reactor模型中 IO事件分发器的核心模块
+class Poller : noncopyable
+{
+    using ChannelMap = std::unordered_map<int, Channel *>;
+
+public:
+    using ChannelList = std::vector<Channel *>;
+    Poller(EventLoop *loop);
+    virtual ~Poller() = 0;
+
+    //调用统一的接口，底层可以为epoll poll select的不同实现
+    virtual TimeStamp Poll(int timeoutMs, ChannelList *activeChannels) = 0;
+
+    virtual void UpdateChannel(Channel *channel) = 0;
+    virtual void RemoveChannel(Channel *channel) = 0;
+
+    //判断channel是否在ChannelMap中
+    bool HasChannel(Channel *channel) const;
+
+    static Poller *NewDefaultPoller(EventLoop *loop);
+
+protected:
+    ChannelMap channels_;
+
+private:
+    EventLoop *ownerLoop_;
+};

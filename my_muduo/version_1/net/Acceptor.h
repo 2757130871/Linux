fa189a3c8a3cb34e7cbad8e0d06e369014bcddf0
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-05-14 21:41:01 +0800

    Create Acceptor.h

---
+++ my_muduo/version_1/net/Acceptor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../base/noncopyable.hpp"
+#include "Socket.h"
+#include "Channel.h"
+
+#include <functional>
+
+class EventLoop;
+class InetAddress;
+
+class Acceptor : noncopyable
+{
+public:
+    using NewConnectionCallBack = std::function<void(int sockfd, const InetAddress &addr)>;
+
+    Acceptor(EventLoop *loop, const InetAddress &listenaddr, bool reuseport = false);
+    ~Acceptor();
+
+    void SetNewConnectionCallBack(const NewConnectionCallBack &cb)
+    {
+        newConnectionCallBack_ = cb;
+    }
+
+    //是否为监听状态
+    bool Listenning() const { return listenning_; }
+
+    void Listen();
+
+private:
+    void HandleRead();
+
+private:
+    EventLoop *loop_; // Acceptor运行在main_loop
+    Socket acceptSocket_;
+    Channel acceptChannel_;
+
+    NewConnectionCallBack newConnectionCallBack_;
+
+    bool listenning_;
+};

1c33f54680a72091baf00201588cb35ca9c892e7
2757130871 <76026098+2757130871@users.noreply.github.com>
2022-05-14 21:40:46 +0800

    Create CallBacks.h

---
+++ my_muduo/version_1/net/CallBacks.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+
+class TimeStamp;
+
+class Buffer;
+class TcpConnection;
+
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
+using ConnectionCallback = std::function<void(const TcpConnectionPtr &)>;
+
+using WriteCompleteCallback = std::function<void(const TcpConnectionPtr &)>;
+using CloseCallback = std::function<void(const TcpConnectionPtr &)>;
+using MessageCallback = std::function<void(const TcpConnectionPtr &, Buffer *, TimeStamp)>;
